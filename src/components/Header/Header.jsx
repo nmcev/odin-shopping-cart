87d9f13ff498b77421af3d79b18ed65d553a31e5
muha <1student.muhammed1@gmail.com>
2024-01-23 18:03:20 +0300

    build: build the header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import  { useState } from 'react';
+import MobileNav from './MobileNav';
+import menu from '../../assets/icons/menu.svg';
+import NavLinks from './NavLinks'
+
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <>
+      <header className="flex justify-between items-center bg-creamy p-6 fixed w-full">
+        <div className="flex items-center pl-2 md:pl-20">
+          <h1  className=" font-header text-2xl text-dimGray">
+            Craft Glow
+          </h1>
+        </div>
+
+        <div className="pr-0 md:pr-56">
+          <button
+            className="md:hidden text-lilac"
+            onClick={toggleMenu}
+            aria-label="Toggle Menu"
+          >
+            <img src={menu} alt="menu" className="w-8 h-8" />
+         </button>
+                  
+          <div className='md:flex hidden'>
+                <NavLinks />
+         </div>
+        </div>
+      </header>
+
+        <MobileNav isOpen={isMenuOpen} onClose={closeMenu} />
+    </>
+  );
+}
+
+export default Header;
