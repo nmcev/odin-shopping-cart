5b52060c702be4d619723ea97d71ccc37475e419
muha <1student.muhammed1@gmail.com>
2024-01-23 18:03:51 +0300

    build: build a nav bar for mobile screen

---
+++ src/components/Header/MobileNav.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import NavLinks from './NavLinks';
+
+const MobileNav = ({ isOpen, onClose }) => {
+  return (
+    <div
+          className={`fixed top-0 left-0 w-full h-80 mt-5.5 bg-black ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'} 
+                    transition-opacity duration-300 ease-in-out`} >
+          
+      <div className="flex justify-end p-4">
+         <button onClick={onClose} className="text-lilac text-2xl" aria-label="Close Menu">
+          &times;
+         </button>      
+      </div>
+
+         <div className='flex justify-center items-center'>
+              <NavLinks />
+         </div>
+    </div>
+  );
+};
+
+MobileNav.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default MobileNav;
