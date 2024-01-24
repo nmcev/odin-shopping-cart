a6a850608dfed2f71e058203ddcce640609abb99
muha <1student.muhammed1@gmail.com>
2024-01-24 20:07:44 +0300

    chore: adding new routers and wrapping the root with chakra

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from './App.jsx'
 import './main.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './components/Home/Home.jsx'
+import { ChakraProvider } from '@chakra-ui/react'
+import Under30 from './components/Products/under30/Under30.jsx'
+import MainProductsPage from './components/Products/MainProductsPage.jsx'
+import AllProducts from './components/Products/allProducts/AllProducts.jsx'
 
 const router = createBrowserRouter([
   {
@@ -13,13 +17,31 @@ const router = createBrowserRouter([
       {
         path: '',
         element: <Home />
-      }
+      },
+      {
+        path: 'products',
+        element: <MainProductsPage />,
+        children: [
+          { 
+            path: '',
+            element: <AllProducts />
+
+          },
+          {
+            path: 'under30',
+            element: <Under30 />
+          }
+        ]
+      },
     ]
     
   }
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+        <ChakraProvider>
+      <RouterProvider router={router} />
+      </ChakraProvider>
+
   </React.StrictMode>,
 )
