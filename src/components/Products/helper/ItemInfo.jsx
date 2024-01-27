504f239f4f1faf4d54fd2b3d92cab08f22324618
muha <1student.muhammed1@gmail.com>
2024-01-27 16:08:22 +0300

    style: make the img more responsive and add more padding

--- src/components/Products/helper/ItemInfo.jsx
+++ src/components/Products/helper/ItemInfo.jsx
@@ -4,9 +4,9 @@ import IncreasingButton from './IncreasingButton'
 function ItemInfo({data, id}) {
 
   return (
-    <div className='flex md:mb-12 mb-9  md:pl-14 md:gap-32 max-sm:flex-col'>
-    <img src={data[id].img} alt="" className='md:max-w-full max-w-44' width={400}/>
-    <div className="md:max-w-96 max-w-48  text-12 gap-y-2 flex flex-col  max-sm:pt-5">
+    <div className='flex md:mb-12 mb-9  xl:pl-52 md:gap-24 md:pl-12 sm:gap-20 max-sm:flex-col'>
+    <img src={data[id].img} alt="" className='sm:max-w-full max-w-44 sm:h-full' />
+    <div className="md:max-w-96 max-w-48 pr-10  text-12 gap-y-2 flex flex-col  max-sm:pt-5">
         <h1 className=' tracking-wider text-18 font-montserrat-reg'>{data[id].name}</h1>
         <div> 
           <span className=' text-dimGray text-16  font-products'>${data[id].price}</span>
