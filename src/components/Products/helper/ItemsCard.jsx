b694efedeb73638c922e2132711ee16975b9f33d
muha <1student.muhammed1@gmail.com>
2024-01-29 00:43:34 +0300

    style: applied the animation on the img tag

--- src/components/Products/helper/ItemsCard.jsx
+++ src/components/Products/helper/ItemsCard.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-
+import './item.css'
 
 function ItemsCard({ img, name, price, id, category }) {
   const url = `/products/${category}/${id}`;
@@ -9,7 +9,7 @@ function ItemsCard({ img, name, price, id, category }) {
 
     <Link to={url}>
       <div className='flex flex-col md:mb-12 mb-9 md:pl-14 items-center'>
-        <img src={img} alt={name} className='md:max-w-full max-w-44' loading='lazy' />
+        <img src={img} alt={name} className='md:max-w-full max-w-44 animate-fade ' loading='lazy' />
         <div className="md:max-w-full max-w-48 flex flex-col items-center text-12 mt-5 gap-y-2 ">
           <h1 className=' tracking-wider text-center'>{name}</h1>
           <span className=' text-dimGray'>{price}$</span>
