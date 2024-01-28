80e6f2a00e4e727525698209bae3d69586981577
muha <1student.muhammed1@gmail.com>
2024-01-28 15:48:46 +0300

    feat: build the page with items that are less than 30 with the items

--- src/components/Products/under30/Under30.jsx
+++ src/components/Products/under30/Under30.jsx
@@ -1,22 +1,29 @@
-import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
 import TitlePage from '../helper/TitlePage'
 import Section from '../helper/Section'
 import SidePageAndItems from '../helper/SidePageAndItems'
+import { Spinner } from '@chakra-ui/react'
+import ItemsCard from '../helper/ItemsCard'
+import ItemsContainer from '../helper/ItemsContainer'
+import ItemInfo from '../helper/ItemInfo';
+import { useCart } from '../../../hooks/CartContext';
 
 function Under30() {
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false)
-    }, 0)
-  }, [])
+  const { id } = useParams(); 
+  const { data30, numberOfItems, handleIncrease, handleDecrease, handleAddToCart, loading } = useCart();
   
-
 
   if (loading) {
     return (
-      <h1 className='grid place-items-center'>Loading...</h1>
+      <div className='grid place-items-center h-dvh'>
+      <Spinner
+       thickness='4px'
+         speed='0.65s'
+         emptyColor='gray.200'
+         color='gray.500'
+        size='xl'
+        />
+        </div>
     )
   }
 
@@ -26,9 +33,33 @@ function Under30() {
      
          <TitlePage />
         <SidePageAndItems>
-          <div className="w-full  flex flex-col gap-6">
+          {/* if the is undefined, it shows all the items. If it not then it will show the element that the user clicked on */}
+          {id === undefined && data30[id] === undefined ? (
+            <ItemsContainer>
+              {
+                data30.map((item) => {
+                  const { name, price, img, id, category } = item;
 
-          </div>
+                  return (
+                    <ItemsCard  name={name} price={price} img={img} id={id} category={category} key={id}/>
+                  )
+                }) 
+              }
+            </ItemsContainer>  
+            
+            ) :
+             data30[id] ? ( 
+              <ItemInfo data={data30} id={id} 
+                handleAddToCart={handleAddToCart}
+                numberOfItems={numberOfItems}
+                handleIncrease={handleIncrease}
+                handleDecrease={handleDecrease}
+              />
+            ) :
+              (
+                <h1 className='grid place-items-center text-20 font-Nunito font-black'>Not Found</h1>
+              )
+        }
         </SidePageAndItems>
         
       </Section>
