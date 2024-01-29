57fbf2694c0ccb0a68f4618d468f10a394915462
muha <1student.muhammed1@gmail.com>
2024-01-29 12:53:00 +0300

    style: change the color of the active link and add paddin for small screens

--- src/components/Products/helper/AsidePage.jsx
+++ src/components/Products/helper/AsidePage.jsx
@@ -9,22 +9,22 @@ function AsidePage() {
 
   return (
     <aside className='flex md:sticky md:top-48'>
-      <div className='md:pl-16 flex md:flex-col gap-1 items-start'>
-        <button className='font-montserrat-reg text-base'>
-          <NavLink to='/products' className={isLinkActive('/products') ? 'text-lime-700 font-semibold' : ''}>
+      <div className='md:pl-16 flex md:flex-col gap-1 items-start max-sm:gap-4 max-sm:items-center max-sm:px-2'>
+        <button className='font-montserrat-reg text-base max-sm:text-14'>
+          <NavLink to='/products' className={isLinkActive('/products') ? 'text-dark font-semibold' : ''}>
             SHOP ALL
           </NavLink>
         </button>
 
-        <ul className='flex md:flex-col gap-2 font-products text-dimGray text-16'>
-          <NavLink to='/products/under30' className={isLinkActive('/products/under30') ? 'text-lime-700 font-semibold' : ''}>
-            under 30
+        <ul className='flex md:flex-col gap-4 font-products text-dimGray text-16 max-sm:text-12'>
+          <NavLink to='/products/under30' className={isLinkActive('/products/under30') ? 'text-dark font-semibold' : ''}>
+            Gifts under 30$
           </NavLink>
-          <NavLink to='/products/under50' className={isLinkActive('/products/under50') ? 'text-lime-700 font-semibold' : ''}>
-            under 50
+          <NavLink to='/products/under50' className={ isLinkActive('/products/under50') ? 'text-dark font-semibold' : ''}>
+            Gifts under 50$
           </NavLink>
-          <NavLink to='/products/under100' className={isLinkActive('/products/under100') ? 'text-lime-700 font-semibold' : ''}>
-            under 100
+          <NavLink to='/products/under100' className={isLinkActive('/products/under100') ? 'text-dark font-semibold' : ''}>
+            Gifts under 100$
           </NavLink>
         </ul>
       </div>
