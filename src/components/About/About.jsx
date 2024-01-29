4510e8bda05013ed105fc6f9370818ce14c6ae4c
muha <1student.muhammed1@gmail.com>
2024-01-29 16:32:12 +0300

    build: add about page

---
+++ src/components/About/About.jsx
@@ -0,0 +1,23 @@
+import { Fade, Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
+const About = () => {
+    return (
+        
+        <div className='flex items-center justify-center w-full pt-24'>
+            <div className=' bg-dimGray h-full p-'>
+
+                <div className='flex flex-col items-center justify-center w-full h-full'>
+                    <h1 className='text-20 text-white'>About</h1>
+                </div>
+                
+
+                <p>
+                </p>
+            </div>
+
+        </div>
+            
+    )
+}
+export default About
