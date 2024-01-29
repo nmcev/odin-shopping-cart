451579eb1401df2e975c8f029c00c10eb326a942
muha <1student.muhammed1@gmail.com>
2024-01-29 16:15:46 +0300

    build: all products page built

--- src/components/Products/allProducts/AllProducts.jsx
+++ src/components/Products/allProducts/AllProducts.jsx
@@ -1,20 +1,59 @@
 import TitlePage from "../helper/TitlePage"
 import Section from "../helper/Section"
 import SidePageAndItems from "../helper/SidePageAndItems"
+import img from "../../../assets/images/allProducts.jpg"
+import { useCart } from "../../../hooks/CartContext"
+import ItemsCard from "../helper/ItemsCard"
+import { v4 as uuidv4 } from 'uuid'
+import ItemsContainer from "../helper/ItemsContainer"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react"
+
 function AllProducts() {
+  const { data30, data50, data100 } = useCart()
+  const data = data30.concat(data50, data100);
+
+  useEffect(() => {
+    AOS.init();
+  }, [])
+  
+  const dataStored = data.sort(() => Math.random() - Math.random()).slice(0, 4);
   const description = 'Explore our curated collection – diverse, versatile items for every need. From essentials to unique finds, discover quality products suitable for gifting.'
     return (
     <Section >
         
-      <TitlePage text="All Products" description={description} />
+    <TitlePage text="All Products" description={description} />
 
-       <SidePageAndItems>
-          <div className=" bg-red-500">
-                <h1>All Products</h1>
+    <SidePageAndItems>
+        <div className="w-10/12 flex justify-center pb-8 relative">
+         <img src={img} alt="Candle and planets" className="w-full max-w-4xl max-h-80 object-cover object-center rounded-lg shadow-lg z-10" loading="lazy" />
+          <div className="absolute inset-0 flex items-center justify-center">
+              <p className="text-creamy z-20 text-2xl font-bold font-poppins bg-black p-2  max-sm:text-lg text-center max-sm:max-w-80">
+                Candle are a great way to bring fragrance into your home.
+              </p>
           </div>
-        </SidePageAndItems>
+          </div>
         
-    </Section>
+          <div className=" text- w-10/12 flex flex-col items-center  max-sm:items-start max-sm:pt-6 max-sm:pl-6 pb-24">
+            <h3 className="text-2xl  font-bold font-poppins text-dimGray max-sm:text-lg ">Our Products</h3>
+            <p className="text-dark max-sm:text-start text-center max-sm:text-sm max-w-2xl pt-2">Explore our curated collection – diverse, versatile items for every need. From essentials to unique finds, discover quality products suitable for gifting.</p>
+          </div>
+
+          <ItemsContainer>
+
+            {dataStored.map((item) => (
+
+              <div key={uuidv4()} data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100" data-aos-once="false">
+                <ItemsCard key={uuidv4()} {...item} />
+              </div>
+            )
+            )}
+
+
+          </ItemsContainer>
+     </SidePageAndItems>  
+  </Section>
   )
 }
 
