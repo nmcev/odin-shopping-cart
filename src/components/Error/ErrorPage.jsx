ca17bd81f280b7adce0ac519deb73eb6a5095547
muha <1student.muhammed1@gmail.com>
2024-01-30 13:12:45 +0300

    style: change the font of the error page and remove the commas

--- src/components/Error/ErrorPage.jsx
+++ src/components/Error/ErrorPage.jsx
@@ -2,10 +2,10 @@ import './style.css'
 function ErrorPage() {
     return (
         <div className="containers flex justify-center flex-col items-center h-dvh">
-            <h1 className="text-9xl font-black text-creamy">404</h1>
-            <h2 className="text-4xl font-black text-creamy">PAGE NOT FOUND</h2>
+            <h1 className="text-9xl text-creamy font-poppins">404</h1>
+            <h2 className="text-4xl  text-creamy font-poppins">PAGE NOT FOUND</h2>
 
-            <p className="text-md font-black text-red-600 pt-7">Sorry, the page you're looking for doesn't exist!</p>
+            <p className="text-md  text-red-600 pt-7 font-poppins">Sorry, the page you&apos;re looking for doesn&apos;t exist!</p>
 
         </div>
   )
