5e61a966d2715a615022762038855377a24261d5
muha <1student.muhammed1@gmail.com>
2024-01-30 14:22:17 +0300

    feat: add the switcher

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,28 @@
 import { Link } from "react-router-dom"
 import './helper/home.css'
+import  Switcher  from '../../Switcher.jsx'
 function Home() {
     return (
-        <main className=" h-dvh ">
+        <main className=" h-dvh  relative ">
         <div className="flex flex-col justify-center items-center h-full ">
           <button className="
             bg-lilac hover:bg-li transition-all duration-300
             text-creamy font-black  py-2 px-4
             hover:text-zinc-700 md:mt-12 mt-8
             md:text-xl font-regPoppins w-48 h-12
+            dark:bg-rose-300 dark:text-dark dark:hover:bg-rose-400 dark:hover:text-creamy
             ">
             <Link to='products' className="text-2xl ">SHOP NOW</Link>
+
+           
           </button>
         </div>
+
+        <div className="flex flex-col items-center transition duration-200  absolute p-2 bottom-4 bg-black">
+        <Switcher />
+
+        </div>
+
       </main>
   )
 }
