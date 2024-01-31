65bf0f8507e58c1dae6593740738f7fd65060a2f
muha <1student.muhammed1@gmail.com>
2024-01-31 12:41:20 +0300

    refactor: update about section with typos and improve it

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,15 +14,14 @@ const About = () => {
 
                     <p className='text-dark font-Nunito text-14 dark:text-creamy'>
                         <span className='text-md font-bold '>Hello, I&apos;m <span className=' font-regPoppins'>Muha</span> </span>
-                        This is the last project that I have done with <span className=' text-purple-400 font-poppins'>The Odin Project</span> on The Front End Development Course especially on ReactJS section. 
+                        . This is the last project that I have done with <span className=' text-purple-400 font-poppins'>The Odin Project</span> on The Front End Development Course especially on ReactJS section. 
                         I have learned a lot of things about ReactJS.
                     </p>
 
                     <section>
                     <h2 className='text-xl text-dark font-bold font-poppins pt-8 text dark:text-creamy'>What I have learned</h2>
                     <p className='text-dark font-Nunito dark:text-creamy'>
-                        This Project learned me a lot of knowledge about ReactJS, especially about React Router, React Hooks, and React Context.
-                        I also know how to use useContext hook and React Router to make a single page application. 
+                    This Project taught me a lot of knowledge about ReactJS, especially about React Router and React Hooks,  I also know how to use the useContext hook and React Router to make a single-page application.
                     </p>
                     <p className='text-dark font-Nunito dark:text-creamy'>
                         I also get familiar with TailwindCSS, a CSS framework that I use to style this project. 
@@ -31,27 +30,23 @@ const About = () => {
                     </section>
 
                     <section>
-                    <h2 className='text-xl text-dark font-bold font-poppins dark:text-creamy'>What is this?</h2>
-                    <p className='text-dark font-Nunito dark:text-creamy'>
-                        This is a website that I build based on TOP curriculum.
-                        It&apos;s a shopping cart website that you can add a product to the cart, remove it, and also change the quantity of the product.
-                        You can also see the total price of the product that you have added to the cart.
-                    </p>
+                        <h2 className='text-xl text-dark font-bold font-poppins dark:text-creamy'>What is this?</h2>
+                        <p className='text-dark font-Nunito dark:text-creamy'>
+                            This is a website that I built based on the TOP curriculum. It&apos;s a shopping cart website where you can add a product to the cart, remove it, and also change the quantity of the product. You can also see the total price of the product that you have added to the cart.
+                        </p>
                     </section>
 
                    <section>
                      <h2 className='text-xl text-dark font-bold font-poppins dark:text-creamy'>Why Candles?</h2>
                      <p className='text-dark font-Nunito dark:text-creamy'>
-                        You may wonder why I choose candles as the product.
-                        The reason is, the last period I was wiling to buy a one for it is amazing smell.
-                        so I picked it as a product because it is fabulous.
+                     You may wonder why I chose candles as the product. The reason is, the last period I was willing to buy one for its amazing smell. so I picked it as a product because it is fabulous.
                      </p>
                     </section>
                     
                     <section>
                         <p className='text-dark font-Nunito leading-7 dark:text-creamy'>
-                            Thanks for <a  className=' underline-offset-2 underline' href="https://unsplash.com/@nixcreative">Tyler Nix</a> for the background image on the home page,
-                            and <a className=' underline-offset-2 underline' href='https://unsplash.com/@pratiksha_mohanty'>Pratiksha Mohanty</a> for the image in the <code className=' bg-zinc-200  dark:bg-slate-500 text-sm py-1'>allProducts</code> page.
+                            Thanks to <a  className=' underline-offset-2 underline' href="https://unsplash.com/@nixcreative">Tyler Nix</a> for the background image on the home page,
+                            and <a className=' underline-offset-2 underline' href='https://unsplash.com/@pratiksha_mohanty'>Pratiksha Mohanty</a> for the image on the <code className=' bg-zinc-200  dark:bg-slate-500 text-sm py-1'>allProducts</code> page.
                         </p>
                     </section>
 
