275c87786cf366234379668627bc8eeb5ba5e326
muha <1student.muhammed1@gmail.com>
2024-02-02 01:21:05 +0300

    style: increase margin between the header and the mobile nav

--- src/components/Header/MobileNav.jsx
+++ src/components/Header/MobileNav.jsx
@@ -4,7 +4,7 @@ import NavLinks from './NavLinks';
 const MobileNav = ({ isOpen, onClose }) => {
   return (
     <div
-          className={`fixed top-0 left-0 w-full h-80 mt-5.5 bg-black z-50 ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'} 
+          className={`fixed top-0 left-0 w-full h-80 mt-24 bg-black z-50 ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'} 
                     transition-opacity duration-300 ease-in-out`} >
           
       <div className="flex justify-end p-4">
