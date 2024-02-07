4a6305ed8e01a8e001783b389e2bed64daa16e38
muha <1student.muhammed1@gmail.com>
2024-02-08 01:39:16 +0300

    feat: implement dynamic icon color based on dark-mode preference

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import  { useState } from 'react';
 import MobileNav from './MobileNav';
-import menu from '../../assets/icons/menu.svg';
 import NavLinks from './NavLinks'
 import { Link } from 'react-router-dom';
 import Switcher from '../../Switcher'
@@ -33,7 +32,7 @@ function Header() {
             onClick={toggleMenu}
             aria-label="Toggle Menu"
           >
-            <img src={menu} alt="menu" className="w-8 h-8" />
+            <svg xmlns="http://www.w3.org/2000/svg"  className='dark:fill-white w-10 h-10'  viewBox="0 -960 960 960"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
          </button>
                   
           <div className='lg:flex hidden  lg: items-center gap-12'>
